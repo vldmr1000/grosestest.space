70cb5648fd1bafd639700acddd590abbb38c0ec5
Groses.space <grosesdorf@outlook.com>
2025-08-24 14:49:17 +0000

    edited web routing

--- resources/views/groses_test.blade.php
+++ resources/views/groses_test.blade.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>test</h1>
+    <h1>Test Page</h1>
+    <a href={{ route('welcome') }}>Link to welcome</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("welcome");
 
 Route::get('/test', function () {
     return view('groses_test');
-});
+})->name("test");
 
