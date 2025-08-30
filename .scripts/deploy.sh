547579108ae5bb16db1d3cd62fb3abbccd42480d
Groses.space <grosesdorf@outlook.com>
2025-08-30 13:57:57 +0000

    edited deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,7 +8,8 @@ echo "Deployment started ... (Деплой начался ...)"
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin main
+#git pull origin main
+git pull
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
